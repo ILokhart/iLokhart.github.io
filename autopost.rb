42f9d4d98021530ab078964dbd12ddc96faaf34f
ilokhart <ilokhart@gmail.com>
2016-02-26 04:49:50 +0000

    remove postest

--- autopost.rb
+++ autopost.rb
@@ -5,17 +5,24 @@ newPost = true
 
 while newPost do
   puts "Enter the title of your post:"
-  @postTitle = gets.chomp
+  postTitle = gets.chomp
 
   puts "Enter the url for an image you'd like to feature:"
-  @postImage = gets.chomp
+  postImage = gets.chomp
 
   puts "Enter the body of your post:"
-  @postBody = gets.chomp
+  postBody = gets.chomp
   
-  `git add . -A`
-  `git commit -m "Added a new post to Post Space"`
-  `git push`
+  blogFile = './postest.html'
+  lineCount = `wc -l "#{blogFile}"`.strip.split(' ')[0].to_i
+  
+  puts blogFile
+  puts lineCount
+  
+  
+  #`git add . -A`
+  #`git commit -m "Added a new post to Post Space"`
+  #`git push`
   
   #Prompt to make another post
   puts "Would you like to make another post? (y or n)"
